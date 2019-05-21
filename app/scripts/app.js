3dbc5ac580b1feccd7e8da35be169e86b1d5192c
Quique Fdez Guerra <quique@ckgrafico.com>
2019-05-21 18:07:15 +0200

    Add some styles

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,8 +2,9 @@ import { createFilePicker } from './file-picker';
 import { processImage } from './image-processor';
 import { generateSandbox } from './sandbox';
 
-const $container = document.querySelector('.js-app');
-createFilePicker($container, image => {
+const $pickerContainer = document.querySelector('.js-picker');
+
+createFilePicker($pickerContainer, image => {
   processImage(image, code => {
     generateSandbox(code, url => {
       window.open(url, '_blank');
